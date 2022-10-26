6f22d8dbdad1f5ab52e23390dbda744ceea6158e
Nam Seob Seo <nsclass@gmail.com>
2022-10-25 21:10:20 -0500

    jdk 19 preview demo

--- src/main/java/org/demo/Main.java
+++ src/main/java/org/demo/Main.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
@@ -24,20 +26,7 @@ public class Main {
         record Failed<T>(Exception exception) implements IResult<T> {}
     }
 
-    record URLData (URL url, byte[] response) { }
-
-    List<URLData> retrieveURLs(List<URL> urls) {
-        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            var tasks = urls
-                    .stream()
-                    .map(url -> executor.submit(() -> fetchUrlData(url)))
-                    .toList();
-            return tasks.stream().map(this::fromFuture)
-                    .map(s -> s instanceof IResult.Success<URLData>(URLData d) ? d : null)
-                    .filter(Objects::nonNull)
-                    .toList();
-        }
-    }
+    record URLData(URL url, byte[] response, long durationMs) { }
 
     IResult<URLData> fromFuture(Future<IResult<URLData>> future) {
         try {
@@ -45,7 +34,7 @@ public class Main {
             return switch(result) {
                 case IResult.Success<URLData> s -> s;
                 case IResult.Failed<URLData> f -> {
-                    System.out.println("failed %s".formatted(f.exception.getMessage()));
+                    System.out.println("failed: %s".formatted(f.exception.getMessage()));
                     yield f;
                 }
             };
@@ -57,7 +46,12 @@ public class Main {
     IResult<URLData> fetchUrlData(URL url) {
         try (InputStream in = url.openStream()) {
             try {
-                return new IResult.Success<>(new URLData(url, in.readAllBytes()));
+                Instant start = Instant.now();
+                System.out.println("started: %s".formatted(url.toString()));
+                var bytes = in.readAllBytes();
+                var duration = Duration.between(start, Instant.now()).toMillis();
+                System.out.println("finished: %s(%dms)".formatted(url.toString(), duration));
+                return new IResult.Success<>(new URLData(url, bytes, duration));
             } catch (Exception e) {
                 return new IResult.Failed<>(e);
             }
@@ -65,11 +59,35 @@ public class Main {
             return new IResult.Failed<>(e);
         }
     }
-    public static void main(String[] args) throws Exception {
+    List<URLData> retrieveURLs(List<URL> urls) {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            var tasks = urls
+                    .stream()
+                    .map(url -> executor.submit(() -> fetchUrlData(url)))
+                    .toList();
+            System.out.println("%d tasks have been created".formatted(tasks.size()));
+            return tasks.stream().map(this::fromFuture)
+                    .map(s -> s instanceof IResult.Success<URLData>(URLData d) ? d : null)
+                    .filter(Objects::nonNull)
+                    .toList();
+        }
+    }
+
+    public static void main(String[] args) {
         List<URL> urls = Stream.of(
                     "https://www.google.com",
-                    "https://www.yahoo.com",
-                    "https://www.youtube.com")
+                        "https://www.youtube.com",
+                        "https://www.yahoo.com",
+                        "https://www.github.com",
+                        "https://www.linkedin.com",
+                        "https://www.amazon.com",
+                        "https://www.bing.com",
+                        "https://www.reddit.com",
+                        "https://www.facebook.com",
+                        "https://www.ebay.com",
+                        "https://www.twitter.com",
+                        "https://www.cloudflare.com",
+                        "https://www.datadoghq.com")
                 .map(url -> {
                     try {
                         return new URL(url);
@@ -79,10 +97,14 @@ public class Main {
                 }).toList();
 
         List<URLData> result = new Main().retrieveURLs(urls);
-        result.forEach(data -> System.out.printf("""
-                url: %s,
-                data: %d
-                %n""", data.url, data.response.length));
+        result.stream()
+            .sorted((x, y) -> Long.compare(x.durationMs, y.durationMs))
+            .forEach(data ->
+                System.out.printf("""
+                    %s, %d(ms) %d(KB)
+                    """, data.url,
+                    data.durationMs,
+                    data.response.length / 1024));
 
     }
 }
